83f27880cc07e53775bdbe7ee18052ef275b2d78
gustavg <gustav.gamstedt@outlook.com>
2026-03-08 20:36:44 +0100

    unoptimized queue sol

--- CF/free/Product_Queries.cpp
+++ CF/free/Product_Queries.cpp
@@ -3,8 +3,11 @@ typedef long long ll;
 typedef unsigned long long ull;
 using namespace std;
 #define rep(i, a, b) for (int i = a; i < b; i++)
+// 1
+// 8
+// 3 2 2 3 7 3 6 7
 
-int f(vector<int> &a, vector<int> &d) {
+int f(vector<int> &a, vector<int> &dist) {
   if (a[0] == 1) {
     d[1] = 1;
   } else {
@@ -18,7 +21,32 @@ int f(vector<int> &a, vector<int> &d) {
 
     rep(j, 2, a.size() + 1) { q.push(j); }
 
-    while
+    int depth = 0; // multiples
+
+    while (!q.empty()) {
+      bool found = false;
+      // I ett visst djup
+      int level_size = q.size();
+      rep(i, 0, level_size) {
+        int d = q.front();
+        q.pop();
+        //
+        if ((a_i % d) != 0) { // fel d som ej delar
+          continue;
+        } else if (dist[a_i] != -1) { // redan mappad;
+          break;
+        } else if (a_i / d == 1) {
+          found = true;
+          break;
+        } else {
+          q.push(a_i / d);
+        }
+      }
+      if (found) {
+        break;
+      }
+      ++depth;
+    }
   }
 
   return 0;
@@ -33,13 +61,13 @@ int main() {
     vector<int> a(n);
     rep(i, 0, n) { cin >> a[i]; }
 
-    vector<int> d(n + 1, -1); // vektor med avstånd till tal
+    vector<int> dist(n + 1, -1); // vektor med avstånd till tal
 
     // Sort and remove duplicates
     sort(a.begin(), a.end());
     auto last = unique(a.begin(), a.end());
     a.erase(last, a.end());
 
-    f(a, d);
+    f(a, dist);
   }
 }
