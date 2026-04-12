6aafe0844d965240f0b663b5196b166bdbe3ddfe
Gustav Gamstedt <Gustav Gamstedt>
2026-04-12 16:35:20 +0200

    idiot_first_search done

--- CF/free/idiot_first_search.cpp
+++ CF/free/idiot_first_search.cpp
@@ -5,48 +5,50 @@ typedef unsigned long long ull;
 using namespace std;
 #define rep(i, a, b) for (int i = a; i < b; i++)
 
-int set_states(ll pos, vector<ll> &L, vector<ll> &R, vector<uint8_t> &S,
-               vector<ll> &T) {
-  if ((!L[pos - 1])) { // leaf
+// This function traverses through the tree and sets the time to go through
+// everything beneith node
+int set_time_part(ll pos, vector<ll> &L, vector<ll> &R, vector<ll> &T) {
+  if (!L[pos - 1]) { // leaf
+    T[pos - 1] = 0;
     return 0;
-  } else if (!S[pos - 1]) { // Nothing
-    S[pos - 1] = 1;         // set left
-    T[pos - 1] = set_states(L[pos - 1], L, R, S, T) + 2;
-
-  } else if (S[pos - 1] == 1) { // Left
-    S[pos - 1] = 2;             // set right
-    T[pos - 1] = set_states(R[pos - 1], L, R, S, T) + 2;
-  } else {          // Right
-    S[pos - 1] = 0; // Set empty
-    return T[pos - 1];
   }
-  return set_states(pos, L, R, S, T) + 2;
+
+  T[pos - 1] += set_time_part(L[pos - 1], L, R, T) + 2;
+  T[pos - 1] += set_time_part(R[pos - 1], L, R, T) + 2;
+  return T[pos - 1];
+}
+
+// This function sets the time for each node to traverse its subtree and larger
+// subtrees above
+void set_time_up(ll prev_val, ll pos, vector<ll> &L, vector<ll> &R,
+                 vector<ll> &T) {
+  T[pos - 1] += prev_val % 1000000007;
+  if (L[pos - 1]) {
+    set_time_up(T[pos - 1] + 1, L[pos - 1], L, R, T);
+    set_time_up(T[pos - 1] + 1, R[pos - 1], L, R, T);
+  }
 }
 
 int main() {
-  ios_base::sync_with_stdio();
+  ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   ll t;
   cin >> t;
   while (t--) {
     ll n;
     cin >> n;
-    vector<ll> L(n, 0), R(n, 0), T(n), P(n);
+    vector<ll> L(n, 0), R(n, 0), T(n);
     // Vectors for left, right, time to run through its nodes and its parent
-    vector<uint8_t> S(n);
-    // State of node. 0 is nothing, 1 is left, 2 is right
     rep(i, 0, n) {
       ll i_L, i_R;
       cin >> i_L >> i_R;
       L[i] = i_L;
       R[i] = i_R;
-      if (i_L)
-        P[i_L] = i + 1;
-      if (i_R)
-        P[i_R] = i + 1;
     }
 
-    set_states(1, L, R, S, T);
-    rep(i, 0, n) { cout << T[i]; }
+    set_time_part(1, L, R, T);
+    set_time_up(1, 1, L, R, T);
+    rep(i, 0, n) { cout << T[i] % 1000000007 << ' '; }
+    cout << '\n';
   }
 }
